1911df0447c0cfe4b6abfad1ec03f3b2be9908cb
fweak1337 <freakgamer2331@gmail.com>
2020-07-04 08:35:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 
 const app = express();
 const PORT = 3001;
 
-
-mongoose.connect('mongodb://localhost:27017/coderingevent1', {
+mongoose.connect("mongodb://localhost:27017/coderingevent1", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(200);
 });
 
@@ -29,5 +29,10 @@ app.get(["/users", "/users/:username"], async (req, res) => {
   res.send({ count: users.length, users: users });
 });
 
+app.get("/posts", async (req, res) => {
+  let posts = await Post.find({});
+
+  res.send({ count: posts.length, posts: posts });
+});
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
