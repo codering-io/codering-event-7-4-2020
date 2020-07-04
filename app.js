4e4723ff075b376f701c051f26279c426bc18112
Anish Shobith P S <anish.shobith19@gmail.com>
2020-07-04 20:12:54 +0530

    add linters and setup workflows

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const User = require("./models/User");
 
 const app = express();
 const PORT = 3001;
 
-mongoose.connect('mongodb://localhost:27017/coderingevent1', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect("mongodb://localhost:27017/coderingevent1", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
-app.get('/', (req, res) => {
-  res.send(200);
+app.get("/", (req, res) => {
+	res.send(200);
 });
 
 app.get("/users", async (req, res) => {
-  let users = await User.find({});
+	const users = await User.find({});
 
-  res.send({ count: users.length, users: users });
+	res.send({ count: users.length, users: users });
 });
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
