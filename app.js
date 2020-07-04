8153e58a63a03ff6a1d23a462303e61be755991b
aspect <airogfx@gmail.com>
2020-07-04 10:37:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./models/User');
+const User = require("./models/User");
 
 const app = express();
 const PORT = 3001;
 
 mongoose.connect('mongodb://localhost:27017/coderingevent1', {
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true
 });
 
 app.get('/', (req, res) => {
   res.send(200);
 });
 
-app.get('/users', (req, res) => {
-  User.find({}, function(err, result) {
+app.get("/users", async (req, res) => {
+  const users = await User.find({});
+  res.send({ count: users.length, users: users });
+});
+
+app.get("/users/:username", async (req, res) => {
+  User.findOne({ username: req.params.username }, async (err, result) => {
     if (err) throw err;
-    res.json(result);
-  })
+    if (!result) return res.send('User not found.')
+    if (result) return res.send(result);
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
