744cce136f9b63edbb8ced8e1d1170c5f7f08c74
Seren_Modz 21 <41450647+SerenModz21@users.noreply.github.com>
2020-07-04 16:59:42 +0100

    Readded all user endpoints in app.js

--- app.js
+++ app.js
@@ -17,28 +17,61 @@ app.get("/", (req, res) => {
   res.send(200);
 });
 
-app.get(["/users", "/users/:username"], async (req, res) => {
-  let { username } = req.params;
+app.get("/users", async (req, res) => {
+  const all = await User.find().select("-password");
+  res.send(all);
+});
 
-  if (username) {
-    let user = await User.findOne({ username: username });
-
-    return res.send(user === null ? { error: `No user with the name: ${username} can be found.` } : user);
-  };
-
-  let users = await User.find({});
-
-  res.send({ count: users.length, users: users });
+app.get("/users/:username", async (req, res) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username }).select("-password");
+  if (!user) return res.status(404).send("No user found!");
+  res.send(user);
 });
 
 app.post("/users", async (req, res) => {
   const { username, password, email } = req.body;
-  if (!username || !password || !email) return res.send({ error: 400, message: "Sorry. But it seems you are missing a piece of information." });
+  if (!username || !password || !email) {
+    return res.status(404).send("Provide a username, email, and password!");
+  }
 
-  const user = new User({ username: username, password: password, email: email });
-  await user.save();
+  const test = new User({ username, email, password });
+  await test.save();
+  res.send(test);
+});
 
-  res.send(user);
+app.put("/users/:user/username", async (req, res) => {
+  const findUser = await findNameOrEmail(req.params.user);
+  if (!findUser) return res.status(404).send("No user found!");
+
+  const { username } = req.body;
+  if (!username) return res.status(404).send("Please provide a username!");
+
+  const checkName = await User.findOne({ email });
+  if (checkName) return res.send("You cannot use this email!");
+
+  await findUser.updateOne({ username });
+  res.sendStatus(200);
+});
+
+app.put("/users/:user/email", async (req, res) => {
+  const findUser = await findNameOrEmail(req.params.user);
+  if (!findUser) return res.status(404).send("No user found!");
+
+  const { email } = req.body;
+  if (!email) return res.status(404).send("Please provide a email!");
+
+  const checkEmail = await User.findOne({ email });
+  if (checkEmail) return res.send("You cannot use this email!");
+
+  await findUser.updateOne({ email });
+  res.sendStatus(200);
+});
+
+app.delete("/users/:user", async (req, res) => {
+  const findUser = await findNameOrEmail(req.params.user);
+  if (!findUser) return res.status(404).send("No user found!");
+  res.send(await findUser.deleteOne());
 });
 
 app.get("/posts", async (req, res) => {
@@ -47,4 +80,15 @@ app.get("/posts", async (req, res) => {
   res.send({ count: posts.length, posts: posts });
 });
 
-app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is listening to requests on Port ${PORT}`)
+});
+
+// Find a user by email or username
+async function findNameOrEmail(value) {
+  const findName = await User.findOne({ username: value });
+  if (findName) return findName;
+  const findEmail = await User.findOne({ email: value });
+  if (findEmail) return findEmail;
+  return null;
+}
