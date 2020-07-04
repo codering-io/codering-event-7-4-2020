6d0cd1078f09bd6b0b3497292dd9c1e6d3868002
creeperplanet26 <fortniteplanet26>
2020-07-04 10:48:59 -0400

    Added GET /posts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 
 const app = express();
 const PORT = 3001;
@@ -24,6 +25,12 @@ app.get("/users:username", async (req, res) => {
   let users = await User.find({ username: req.params.username });
 
   res.send({ count: users.length, users: users });
+});
+
+app.get("/posts", async (req, res) => {
+  let Posts = await Post.find({});
+
+  res.send({ count: posts.length, posts: posts });
 });
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
