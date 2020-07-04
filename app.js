4a2094185539c1a9b83374f09be7222b1c1be272
aspect <airogfx@gmail.com>
2020-07-04 11:41:15 -0400

    Revert "Revert "Merge branch 'master' into master""
    This reverts commit 7c13a4ad47003771a805ee7826a09562cbd171b2.

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const User = require("./models/User");
 const app = express();
 const PORT = 3001;
 
-mongoose.connect('mongodb://localhost:27017/coderingevent1', {
+mongoose.connect("mongodb://localhost:27017/coderingevent1", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(200);
 });
 
