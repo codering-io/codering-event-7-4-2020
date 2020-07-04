bf43410ddd51c4dd0962e969729f1d75b92eea83
aspect <airogfx@gmail.com>
2020-07-04 10:10:58 -0400

    Finished /users.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +12,13 @@ mongoose.connect('mongodb://localhost:27017/coderingevent1', {
 
 app.get('/', (req, res) => {
   res.send(200);
+});
+
+app.get('/users', (req, res) => {
+  User.find({}, function(err, result) {
+    if (err) throw err;
+    res.json(result);
+  })
 });
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
