afd84da69d985cd3846c151262f2a4622fa0325e
creeperplanet26 <fortniteplanet26>
2020-07-04 10:58:25 -0400

    added get posts

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 
 const app = express();
 const PORT = 3001;
 
-mongoose.connect('mongodb://localhost:27017/coderingevent1', {
+mongoose.connect("mongodb://localhost:27017/coderingevent1", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(200);
 });
 
@@ -18,6 +19,12 @@ app.get("/users", async (req, res) => {
   let users = await User.find({});
 
   res.send({ count: users.length, users: users });
+});
+
+app.get("/posts", async (req, res) => {
+  let Posts = await Post.find({});
+
+  res.send({ count: posts.length, posts: posts });
 });
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
