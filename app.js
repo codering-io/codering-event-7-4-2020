79355590111067d36d33f63f0a26f16dcef23a06
creeperplanet26 <fortniteplanet26>
2020-07-04 10:43:31 -0400

    added get user by username

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const User = require("./models/User");
 
 const app = express();
 const PORT = 3001;
 
-mongoose.connect('mongodb://localhost:27017/coderingevent1', {
+mongoose.connect("mongodb://localhost:27017/coderingevent1", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(200);
 });
 
 app.get("/users", async (req, res) => {
   let users = await User.find({});
+
+  res.send({ count: users.length, users: users });
+});
+
+app.get("/users:username", async (req, res) => {
+  let users = await User.find({ username: req.params.username });
 
   res.send({ count: users.length, users: users });
 });
