72c75521f323a5bb620eac3c304fc548bda6daf9
[Cloud Chaser] Freak <33170633+Freak-chan@users.noreply.github.com>
2020-07-04 06:57:47 -0700

    First Get Endpoint - Users

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require("./models/User");
 
 const app = express();
 const PORT = 3001;
 
 mongoose.connect('mongodb://localhost:27017/coderingevent1', {
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true
 });
 
 app.get('/', (req, res) => {
   res.send(200);
+});
+
+app.get("/users", (req, res) => {
+  User.find({}, (error, results) => {
+    if (error) res.send({ message: "An error occured", error: error.stack });
+
+    res.send(results);
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is listening to requests on Port ${PORT}`));
