e37f7cd9914604bff1cf7b632674cbbfdae618ff
[Cloud Chaser] Freak <33170633+Freak-chan@users.noreply.github.com>
2020-07-04 11:11:38 -0700

    Fixed POST /posts ( checks for same name/email )

--- app.js
+++ app.js
@@ -93,6 +93,10 @@ app.post("/posts", async (req, res) => {
   const { username, email, post: { title, content } } = req.body;
   if (!username || !email || !title || !content) return res.send({ error: 400, message: "Sorry. But it seems you are missing a piece of information." });
 
+  let user = await findNameOrEmail(username);
+  if (user === null) return res.send({ error: 400, message: "Sorry but this user doesn't exist in our database" });
+  if (user.email !== email) return res.send({ error: 401, message: "Sorry but the email does not correspond to the user." });
+
   const post = new Post({ title: title, content: content, author: username, createdOn: new Date(), editedOn: null });
   await post.save();
 
